4ceac303c5e1839e61c996c6891fa475a66550ce
jpenrrami <jpenrrami@gmail.com>
2024-02-20 19:45:58 +0100

    cambio

---
+++ index.js
@@ -0,0 +1,9 @@
+let cool = require("cool-ascii-faces");
+let express = require("express")
+
+let app = express();
+
+app.listen(10000);
+
+///console.log(cool());
+console.log("Server listening on port 10000");
