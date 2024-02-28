11b50ca455a8b6678c8fe20d947c0b71e215c6de
jpenrrami <jpenrrami@gmail.com>
2024-02-28 14:58:46 +0100

    datos en api jpr

--- api/index-JPR.js
+++ api/index-JPR.js
@@ -2,7 +2,7 @@ const API_BASE="/api/v1";
 let data_JPR= require('../index-JPR');
 
 module.exports = (app) => {
-    app.get(API_BASE+"/esif-2014-2020-eu-payments", (req,res)=>{
+    app.get(API_BASE+"/esif-payments", (req,res)=>{
         res.send(JSON.stringify(data_JPR));
     });
 }
