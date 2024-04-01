15288a9e0c7704100171cbce7d80333b9b1c3c3a
Alberto Fraile Infantes <albfrainf@alum.us>
2024-04-01 09:32:12 +0200

    DFF

--- front/src/routes/policy-program-stats/+page.svelte
+++ front/src/routes/policy-program-stats/+page.svelte
@@ -435,12 +435,6 @@
             <button
                 style="background-color: #FF0000; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;"
                 on:click={() => {
-                     DeleteAllStats();
-                }}>Eliminar Todos Los Datos
-            </button>
-            <button
-                style="background-color: #FF0000; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;"
-                on:click={() => {
                     DeleteAllStats();
                 }}>Eliminar Todos</button
             >
