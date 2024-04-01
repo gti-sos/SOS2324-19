6fb32629d634db6f157080ac02dbdd7103d069d8
Alberto Fraile Infantes <albfrainf@alum.us>
2024-04-01 08:54:44 +0200

    gg

--- front/src/routes/policy-program-stats/+page.svelte
+++ front/src/routes/policy-program-stats/+page.svelte
@@ -761,12 +761,12 @@
 		width: 100%;
 		height: 100%;
 		overflow: auto;
-		background-color: rgba(0, 0, 0, 0.4); /* Fondo oscuro */
+		background-color: rgba(0, 0, 0, 0.4); 
 	}
 
 	.modal-content {
-		background-color: #fefefe; /* Color de fondo */
-		margin: 15% auto; /* Centrar el popup verticalmente */
+		background-color: #fefefe;
+		margin: 15% auto;
 		padding: 20px;
 		border: 1px solid #888;
 		width: 50%;
