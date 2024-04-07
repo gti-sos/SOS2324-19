715f53ddbdce7f2650ebe34f907932d426bde8d1
jpenrrami <jpenrrami@gmail.com>
2024-04-07 13:33:55 +0200

    implementacion de paginacion y offset

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -41,12 +41,7 @@ module.exports = defineConfig({
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    }
 
     /* Test against mobile viewports. */
     // {
---
+++ tests/e2e/JPR.spec.cjs
@@ -0,0 +1,24 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:10000');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Menú de Navegación/);
+});
+
+
+test('list countries', async ({ page }) => {
+  await page.goto('http://localhost:10000/esif-payments');
+
+  // Click the get started link.
+  await page.click('#cargarDatos');
+
+  // Expects page to have a heading with the name of Installation.
+  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  let countries= (await page.locator('.countryItem').all())
+  let coutriesNumber = countries.length;
+
+  expect(coutriesNumber).toBeGreaterThan(0);
+});
