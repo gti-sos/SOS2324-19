b2b5e099e79025c4df9ef6f051f68d7a60fc8a78
Alberto Fraile Infantes <albfrainf@alum.us>
2024-04-19 19:55:40 +0200

    fff

--- front/src/routes/policy-program-stats/graphs/+page.svelte
+++ front/src/routes/policy-program-stats/graphs/+page.svelte
@@ -1,12 +1,15 @@
 <svelte:head>
     <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-more.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
     <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+    <script src="https://code.highcharts.com/modules/treemap.js"></script>
+    <script src="https://code.highcharts.com/modules/heatmap.js"></script>
 </svelte:head>
 
 <script>
-    import {onDestroy, onMount} from "svelte";
+    import {onMount} from "svelte";
 
     onMount(() => {
         getData();
@@ -24,106 +27,114 @@
             if (data.length > 0) {
                 dataAvailable = true; 
                 createGraph(data);
+                createg2(data);
             }
         }catch(error){
             console.log(`Error fetching data: ${error}`);
         }
     }   
-    function createGraph(data){
-        const atData = data.filter(item => item.country === "AT");
-        const beData = data.filter(item => item.country === "BE");
-        Highcharts.chart('container-bar', {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: 'Cantidad monetaria pagada por cada pais en cada año ',
-        align: 'center'
-    },
-    xAxis: {
-        categories:[2015,2016,2017,2018,2019,2020,2021,2022,2023,2024],
-        crosshair: true,
-        accessibility: {
-            description: 'Countries'
-        }
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Cantidad total monetaria pagada al fondo'
-        }
-    },
-    tooltip: {
-        headerFormat: '<b>{point.x}</b><br/>',
-        pointFormat: '{series.name}: {point.y} €'
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [
-        {
-            name: 'AT',
-            data: atData.map(item => parseFloat(item.total_amount_paid_to_fi))
+        function createGraph(data){
+                const atData = data.filter(item => item.country === "AT");
+                const beData = data.filter(item => item.country === "BE");
+                Highcharts.chart('container-bar', {
+            chart: {
+                type: 'column'
             },
-            {
-            name: 'BE',
-            data: beData.map(item => parseFloat(item.total_amount_paid_to_fi))
-            }
-    ]
-});
+            title: {
+                text: 'Monetary amount paid by each country in each year',
+                align: 'center'
+            },
+            xAxis: {
+                categories:[2015,2016,2017,2018,2019,2020,2021,2022,2023,2024],
+                crosshair: true,
+                accessibility: {
+                    description: 'Countries'
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Total monetary amount paid to the fund'
+                }
+            },
+            tooltip: {
+                headerFormat: '<b>{point.x}</b><br/>',
+                pointFormat: '{series.name}: {point.y} €'
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [
+                {
+                    name: 'AT',
+                    data: atData.map(item => parseFloat(item.total_amount_paid_to_fi))
+                    },
+                    {
+                    name: 'BE',
+                    data: beData.map(item => parseFloat(item.total_amount_paid_to_fi))
+                    }
+            ]
+        });
+}
 
-        // const gr = Highcharts.chart('container-bar', {
-        //     chart: {
-        //         type: 'line'
-        //     },
-        //     title: {
-        //         text: 'Cantidad monetaria pagada por cada pais en cada año ',
-        //         align: 'center'
-        //     },
-        //     xAxis: {
-        //         categories:[2015,2016,2017,2018,2019,2020,2021,2022,2023,2024]
-        //     },
-        //     yAxis: {
-        //         title: {
-        //             text: 'Cantidad total monetaria pagada al fondo'
-        //         },
-        //         stackLabels: { // Habilitar etiquetas de apilamiento para totales
-        //         enabled: true
-        //     }
-        //     },
-        //     tooltip: {
-        //         headerFormat: '<b>{point.x}</b><br/>',
-        //         pointFormat: '{series.name}: {point.y} €'
-        //     },
-        //     plotOptions: {
-        //         line: {
-        //             dataLabels: {
-        //                 enabled: true
-        //             },
-        //             enableMouseTracking: false
-        //         }
-        //     },
-        //     series: [{
-        //               name: 'AT',
-        //         data: atData.map(item => parseFloat(item.total_amount_paid_to_fi))
-        //         },
-        //         {
-        //               name: 'BE',
-        //         data: beData.map(item => parseFloat(item.total_amount_paid_to_fi))
-        //         }
-        //     ]
-        // });
+    function createg2(data){
+        const tiposPrioridad = [...new Set(data.map(item => item.priority))];
 
-
-
-
+            Highcharts.chart('container', {
+                chart: {
+                    type: 'packedbubble'
+                },
+                title: {
+                    text: 'Average total amount committed to the fund by Priority',
+                    align: 'center'
+                },
+                tooltip: {
+                    useHTML: true,
+                    pointFormat: '<b>{point.name}:</b> {point.value}'
+                },
+                plotOptions: {
+                            packedbubble: {
+                                minSize: '13%',
+                                maxSize: '35%',
+                                zMin: 0,
+                                zMax: 300,
+                                layoutAlgorithm: {
+                                    gravitationalConstant: 0.05,
+                                    splitSeries: true,
+                                    seriesInteraction: false,
+                                    dragBetweenSeries: true,
+                                    parentNodeLimit: true
+                                },
+                                dataLabels: {
+                                    enabled: true,
+                                    format: '{point.name}',
+                                    filter: {
+                                        property: 'y',
+                                        operator: '>',
+                                        value: 0
+                                    },
+                                    style: {
+                                        color: 'black',
+                                        textOutline: 'none',
+                                        fontWeight: 'normal'
+                                    }
+                                }
+                            }
+                        },
+                series: tiposPrioridad.map(prioridad => ({
+                    name: prioridad,
+                    data: data.filter(item => item.priority === prioridad).map(item => ({
+                        name: item.country,
+                        value: parseFloat(item.total_amount_committed_to_fi)
+                    }))
+                }))
+            });
     }
-
 </script>
 
-<div id="container-column"></div>
+<div id="container"></div>
 <br>
 <div id="container-bar"></div>
